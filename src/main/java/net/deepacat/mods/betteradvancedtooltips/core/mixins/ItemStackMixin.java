98e06c6d2fb852c236bbf5c5b3c3cbcf60c1e6a1
Deepa <52133154+Deepacat@users.noreply.github.com>
2026-05-29 21:14:23 -0500

    Fix mixin removing all hasTag() entries from tooltips

--- src/main/java/net/deepacat/mods/betteradvancedtooltips/core/mixins/ItemStackMixin.java
+++ src/main/java/net/deepacat/mods/betteradvancedtooltips/core/mixins/ItemStackMixin.java
@@ -1,22 +1,31 @@
 package net.deepacat.mods.betteradvancedtooltips.core.mixins;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.deepacat.mods.betteradvancedtooltips.Config;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.contents.TranslatableContents;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-// Disables the "NBT Tags: x" on tooltips
+import java.util.List;
+
+// Removes the "NBT Tags: x" text from tooltips
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
-	@ModifyExpressionValue(
-		method = "getTooltipLines",
-		at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;hasTag()Z")
-	)
-	private boolean bat$getTooltipLines(boolean original) {
-		if (original && Config.removeComponentCountTooltip.get()) {
-			return false;
+	@Inject(method = "getTooltipLines", at = @At("RETURN"))
+	private void bat$postProcessTooltip(Player pPlayer, TooltipFlag pIsAdvanced, CallbackInfoReturnable<List<Component>> cir) {
+		if (Config.removeComponentCountTooltip.get()) {
+			List<Component> tooltip = cir.getReturnValue();
+			tooltip.removeIf(component -> {
+				if (component.getContents() instanceof TranslatableContents translatableContents) {
+					return "item.nbt_tags".equals(translatableContents.getKey());
+				}
+				return false;
+			});
 		}
-		return original;
 	}
 }
